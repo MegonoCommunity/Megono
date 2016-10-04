cc3c56cf805a236c699a789de2359ad27fd6c707
virusphp <isi_dengan_email_anda@ymail.com>
2016-10-05 00:58:11 +0700

    menambahkan view core

---
+++ library/view.class.php
@@ -0,0 +1,46 @@
+<?php 
+class View
+{	
+	//membuat property
+	public $namaView = NULL;
+	public $data = array();
+	public $isRender = FALSE;
+	//membuat method constructor yang di jalankan saat object di buat
+	public function __construct($view)
+	{
+		$this->namaView = $view;
+	}
+
+	//membuat method/fungsi bind view
+	public function bind($nama, $value = '')
+	{
+		if(is_array($nama))
+		{
+			foreach ($nama as $attr => $val) 
+			{
+				$this->data[$attr] = $val;
+			}
+		}
+		else $this->data[$nama] = $value;
+	}
+
+	//membuat method/fungsi forceRender / memaksa rendering
+	public function forceRender()
+	{
+		$this->isRender = TRUE;
+		extract($this->data);
+		$view = ROOT . DS . 'modules' . DS . 'views' . DS . $this->namaView . 'view.php';
+		//melakukan pengecekan file view
+		if(file_exists($view)) require_once $view;
+		else echo('View tidak di temukan !');
+	}
+
+	//membuat fungsi destruct menghacurkan dan mengeksekusi pada akhir data
+	public function __destruct()
+	{
+		if(! $this->isRender) $this->forceRender();
+	}
+
+}
+
+ ?>
