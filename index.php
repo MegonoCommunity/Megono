12f78e0e250ac0eb8358d7d905e6b4caefe43128
virusphp <virus_php@yahoo.com>
2016-10-04 12:09:34 +0700

    penambahan index.php dan merubah nama variable menjadi bahasa indonesia

--- index.php
+++ index.php
@@ -17,12 +17,49 @@ require_once "library/controller.class.php";
 function __autoload($namaKelas)
 {
 	//menseting nama kelas dengan menghapus tanda dan separator serta .php
-	$namaKelas = str_replace("\\", DS, $namaKelass) . '.php';
-	if(!file_exists($namaKelas))
+	$namaFile = str_replace("\\", DS, $namaKelas) . '.php';
+	if(!file_exists($namaFile))
 	{
 		return false;
 	}
-	include $namaKelas;
+	include $namaFile;
 }
+
+//membuat sintak untuk sistem MVC
+$page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : 'home';
+//mendefinisikan sebuah controller terletak pada /root(namafilekita)/modules/controllers/controller.php
+$controller = ROOT . DS . 'modules' . DS . 'controllers' . DS . $page . 'Controller.php';
+
+//melakukan pengecekan file controller
+//jika controller ada
+if (file_exists($controller))
+{
+	//melakukan require controller
+	require_once $controller;
+	//aksi mendapatkan aksi dan aksi mendapatkan url sesuai yang di minta
+	$aksi = (isset($_GET['aksi']) && $_GET['aksi']) ? $_GET['aksi'] : 'index';
+	//mendefinisikan nama controller
+	$namaKontroller = ucfirst($page) . 'Controller';
+	//membuat instansiasi atau object
+	$obj = new $namaKontroller();
+	//melakukan pengecekan method objek
+	if(method_exists($obj, $aksi))
+	{
+		//sett args menjadi array
+		$args = array();
+		if(count($_GET) > 2)
+		{
+			$bagian = array_slice($_GET, 2);
+
+			foreach ($bagian as $bag) 
+			{
+				array_push($args, $bag);
+			}
+		}
+		call_user_func_array(array($obj, $aksi), $args);
+	}
+	else die(' Aksi tidak di temukan !');
+}
+else die(' Controller tidak di temukan')
 
  ?>
